1d12f42a579cd44e15b12b19640b402a1ff9a744
Jacob Rainbow <46567925+JRainbowOS@users.noreply.github.com>
2020-04-05 19:00:19 +0100

    try to animate multiple random walks

--- grid.py
+++ grid.py
@@ -13,15 +13,25 @@ class Grid(Tile):
         self.coord_system = Grid.COORD_SYSTEM
 
     @classmethod
-    def to_toroidal(cls, coord: Cartesian2D):
+    def cartesian_to_toroidal(cls, coord: Cartesian2D):
         x_in, y_in = coord.x, coord.y
         x_out, y_out = x_in % cls.GRIDSIZE, y_in % cls.GRIDSIZE
         new_coord = Cartesian2D(x_out, y_out)
         return new_coord
 
     @classmethod
+    def float_to_toroidal(cls, coord: float):
+        new_coord = coord % cls.GRIDSIZE
+        return new_coord
+
+    @classmethod
     def coord_list_to_toroidal(cls, coord_list: List[Cartesian2D]):
         toroidal_list = [cls.to_toroidal(coord) for coord in coord_list]
+        return toroidal_list
+
+    @classmethod
+    def trace_list_to_toroidal(cls, trace_list: List[float]):
+        toroidal_list = [cls.float_to_toroidal(coord) for coord in trace_list]
         return toroidal_list
 
     def draw(self):
@@ -31,9 +41,11 @@ class Grid(Tile):
 def main():
 
     grid = Grid()
-    in_coords = [Cartesian2D(12, -14), Cartesian2D(3, 19)]
-    out_coords = grid.coord_list_to_toroidal(in_coords)
-    print(out_coords)
+    # in_coords = [Cartesian2D(12, -14), Cartesian2D(3, 19)]
+    # out_coords = grid.coord_list_to_toroidal(in_coords)
+    in_trace = [2,4,7,13,-4]
+    out_trace = grid.trace_list_to_toroidal(in_trace)
+    print(out_trace)
     # grid.draw()
 
 if __name__ == '__main__':
--- outbreak.py
+++ outbreak.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from typing import List
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from scenario import Scenario
 
@@ -12,38 +13,85 @@ from cartesian import Cartesian2D
 
 class Outbreak(Scenario):
 
+    DT = 1 
+
     def __init__(self,
                  grid: Grid,
                  community: Community):
         self.grid = grid
         self.community = community
+        self._ids = []
+        self.toroidal_trace_dict_x = {}
+        self.toroidal_trace_dict_y = {}
 
     def evolve(self):
         self.community.multistep()
 
-    def plot_traces(self):
+    def toroidal_trace(self, plot=False):
         for _id, person in self.community.host_dict.items():
-            tr = person.trace
-            # print(tr)
-            new_tr = self.grid.coord_list_to_toroidal(tr)
-            s = [coord.x for coord in new_tr]
-            # print(f'trace for id \t {_id}: {s}')
-            # t = [coord.y for coord in new_tr]
-            # plt.plot(s, t) 
-        # plt.show()
+            s = self.grid.trace_list_to_toroidal(person.trace_x)
+            self.toroidal_trace_dict_x[_id] = s
+            t = self.grid.trace_list_to_toroidal(person.trace_y)
+            self.toroidal_trace_dict_y[_id] = t
+            self._ids.append(_id)
+            if plot:
+                plt.scatter(s, t)
+        if plot:
+            plt.show()
 
-    def sketch(self):
-        for _id, person in self.community.host_dict.items():
-            s = person.trace_x
-            t = person.trace_y
-            plt.plot(s, t)
+    def animate(self):
+        """
+        Produce an animation of the pursuit processed so far
+        """
+        dt = Outbreak.DT
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, autoscale_on=False, xlim=(0, self.grid.size),
+                                                      ylim=(0, self.grid.size))
+        ax.set_aspect('equal')
+
+        time_template = 'time = %.1fs'
+        time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+        
+        community_xdata = {}
+        community_ydata = {}
+        lines = []
+        for _id in self._ids:
+            person_xdata, person_ydata = [], []
+            community_xdata[_id] = person_xdata
+            community_ydata[_id] = person_ydata
+            ln, = ax.plot([], [], 'ro', linewidth=2, markersize=1)
+            lines.append(ln)
+
+        def init_animation():
+            # for _id in self._ids:
+            #     ln = community_ln[_id]
+            #     ln.set_data([], [])
+            for ln in lines:
+                ln.set_data([], [])
+            time_text.set_text('')
+            return lines[0], time_text
+
+        def animate(i):
+            for i, _id in enumerate(self._ids):
+                community_xdata[_id].append(self.toroidal_trace_dict_x[_id][i])
+                community_ydata[_id].append(self.toroidal_trace_dict_y[_id][i])
+                lines[i].set_data(community_xdata[_id], community_ydata[_id])
+
+            time_text.set_text(time_template % (i * dt))
+            return lines[0], time_text
+            # return ln_chaser, ln_chased, time_text
+
+        FuncAnimation(fig, 
+                      animate,
+                      len(self.toroidal_trace_dict_x[_id]), # not very nice!
+                      init_func=init_animation,
+                      interval=500,
+                      blit=True,
+                      repeat=False)
         plt.show()
 
 
-            # for pt in tr:
-
-            #     toroidal_pt = self.grid.toroidal(pt)
-            #     traces.append()
 
 def person_generator(grid_size):
     i = 1
@@ -60,6 +108,7 @@ def main():
     
     NUM_PEOPLE = 10  
     GRID_SIZE = 5  
+    TIME_STEPS = 5
 
     pg = person_generator(GRID_SIZE)
     people = [next(pg) for _ in range(NUM_PEOPLE)]
@@ -69,10 +118,13 @@ def main():
     
     ob = Outbreak(grd, com)
 
-    for _ in range(10):
+    for _ in range(TIME_STEPS):
         ob.evolve()
 
-    ob.sketch()
+    ob.toroidal_trace(plot=False)
+    # print(ob.toroidal_trace_dict_x)
+    # print(ob._ids)
+    ob.animate()
 
 if __name__ == '__main__':
     main()
